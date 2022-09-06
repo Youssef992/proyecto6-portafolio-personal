7360ca21ff37c7a9273c9404284fab4531fa30f7
Manuel Youssef Morales Matta <mmorales@dell1425.tbn.com.mx>
2022-09-06 13:31:56 -0500

    Se agregaron las insignias y tecnologías aprendidas

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,11 +1,14 @@
 import "./about.css";
 import Jigglypuff from "../../img/Jigglypuff.png";
-
+import Cine from "../../img/cine.png";
+import Gaming from "../../img/gaming.png";
+import Gym from "../../img/gym.png";
+import Programmer from "../../img/programmer-icon.png";
+import Smash from "../../img/smash.png";
 const About = () => {
     return (
         <div className="a">
             <div className="a-left">
-
                 <div className="a-card">
                     <img
                         src={Jigglypuff}
@@ -17,20 +20,19 @@ const About = () => {
             <div className="a-right">
                 <h1 className="a-title">Sobre Mi</h1>
                 <p className="a-sub">
-                    Actualmente tengo 30 años de edad. Trabajo en una empresa de Facturación Electrónica llamada: "Digifact". Llevo trabajando 5 años, recién cumplidos.
+                    Tengo 30 años de edad. Trabajo en una empresa de Facturación Electrónica.
                 </p>
                 <br/>
-                <p className="a-desc">
-                    Me apasionan los videojuegos. Hoy en día me dedico a ser jugador profesional de Super Smash Bros. Ultimate.
-                </p>
-                <br/>
-                <p className="a-desc">
-                    Una de mis metas a cort-mediano plazo es convertirme en freelancer, ya que aprendería mucho a lo largo del tiempo, con nuevos retos que se presenten día a día, desarrollando proyectos nuevos.
-                </p>
-                <br/>
-                <p className="a-desc">
-                    A nivel personal me encantaría poner una cafetería con temática de Pokémon, dedicado a la repostería, ya que ésta es uno de mis pasatiempos
-                </p>
+                <h1 className="a-title">Hobbies</h1>
+                <div className="container">
+                    <img src={Cine} alt="" className="img-hobbies" />
+                    <img src={Gaming} alt="" className="img-hobbies" />
+                    <img src={Gym} alt="" className="img-hobbies" />
+                    <img src={Programmer} alt="" className="img-hobbies" />
+                    <img src={Smash} alt="" className="img-hobbies" />
+                </div>
+            
+               
 
             </div>
         </div>
--- src/components/Proyects/Proyects.jsx
+++ src/components/Proyects/Proyects.jsx
@@ -3,13 +3,23 @@ import Proyecto1 from "../../img/Proyecto1.PNG"
 import Proyecto3 from "../../img/Proyecto3.PNG"
 import Proyecto4 from "../../img/Proyecto4.PNG"
 import Proyecto5 from "../../img/Proyecto5.PNG"
+import LandingPage from "../../img/landing-pages-sobresaliente.1.png"
+import CRUD from "../../img/aplicacion-crud-sobresaliente.png"
+import Dashboard from "../../img/dashboard-sobresaliente.png"
+import Restaurante from "../../img/restaurante-app-sobresaliente.png"
+import HTML from "../../img/html5.png"
+import Bootstrap from "../../img/bootstrap.png"
+import JavaScript from "../../img/javascript.png"
+import MongoDB from "../../img/mongo.png"
+import NodeJS from "../../img/Node.png"
+import ReactIMG from "../../img/react.png"
 import "../Proyect/proyect.css";
 const Proyects = () => {
-  return (
+  return (<>
     <div className="pl">
       <div className="pl-texts">
         <h1 className="pl-title">Proyectos BootCamp</h1>
-        <p className="pl-desc">
+        <p className="pl-desc"> 
           En esta sección se encuentran los proyectos desarrollados a lo largo del curso.
         </p>
       </div>
@@ -37,7 +47,6 @@ const Proyects = () => {
           </a>
           <h5>Proyecto 2- CRUD</h5>
           <p style={{fontSize:"11px"}}>Descripción: En este proyecto se creó un landing page utilizando HTML y CSS agregando CRUD</p>
-       
         </div>
         <div className="p">
           <div className="p-browser">
@@ -66,6 +75,64 @@ const Proyects = () => {
         </div>
       </div>
     </div>
+
+    <div className="pl">
+      <div className="pl-texts">
+        <h1 className="pl-title">Insignias BootCamp FullStack</h1>
+        <p className="pl-desc">
+          En esta sección se encuentran las insignias obtenidas a lo largo del Bootcamp.
+        </p>
+      </div>
+      <div className="pl-list">
+        <div className="p">
+          <h3>Proyecto 1</h3>
+          <img src={LandingPage} alt='Landing Page'/>
+        </div>
+        <div className="p">
+          <h3>Proyecto 2</h3>
+          <img src={CRUD} alt='CRUD'/>
+        </div>
+        <div className="p">
+          <h3>Proyecto 3</h3>
+          <img src={Dashboard} alt='Dashboard'/>
+        </div>
+        <div className="p">
+          <h3>Proyecto 4</h3>
+          <img src={Restaurante} alt='Restaurante'/>
+        </div>
+      </div>
+    </div>
+
+    <div className="pl">
+      <div className="pl-texts">
+        <h1 className="pl-title">Tecnologías Utilizadas</h1>
+        <p className="pl-desc">
+          En esta sección se encuentran las tecnologías utilizadas y aprendidas durante el BootCamp.
+        </p>
+      </div>
+      <div className="pl-list">
+        <div className="p">
+          <img src={HTML} style={{width:'150px',height:'150px'}} alt='HTML'/>
+        </div>
+        <div className="p">
+          <img src={Bootstrap} style={{width:'150px',height:'150px'}} alt='Bootstrap'/>
+        </div>
+        <div className="p">
+          <img src={JavaScript} style={{width:'150px',height:'150px'}} alt='JavaScript'/>
+        </div>
+        <div className="p">
+          <img src={MongoDB} style={{width:'150px',height:'150px'}} alt='MongoDB'/>
+        </div>
+        <div className="p">
+          <img src={NodeJS} style={{width:'150px',height:'150px'}} alt='NodeJS'/>
+        </div>
+        <div className="p">
+          <img src={ReactIMG} style={{width:'150px',height:'150px'}} alt='React'/>
+        </div>
+      </div>
+    </div>
+    </>
+    
   );
 };
 
